08b9693322a066a2728c5fa1d3bf4cc0015d6944
cristians-12 <cristian.sanchez.casd@gmail.com>
2025-11-25 15:42:23 -0500

    -create employee model -create docker file

--- src/main/java/com/cristians12/spring/learning_spring/controllers/RestExampleController.java
+++ src/main/java/com/cristians12/spring/learning_spring/controllers/RestExampleController.java
@@ -4,16 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cristians12.spring.learning_spring.models.Employee;
+
 @RestController
+@RequestMapping("/api")
 public class RestExampleController {
 
-    @GetMapping("/employees")
+    // @GetMapping("/employees")
+    @RequestMapping(path = "/employees", method = RequestMethod.GET)
     public Map<String, Object> get() {
+        Employee employee1 = new Employee("Cristian", "Sanchez", "Cra 19D", "Developer", 3000, 24);
         Map<String, Object> response = new HashMap();
-        response.put("nombre", "Cristian");
-        response.put("edad", 24);
+        response.put("employee", employee1);
         return response;
     }
 
---
+++ src/main/java/com/cristians12/spring/learning_spring/models/Employee.java
@@ -0,0 +1,63 @@
+package com.cristians12.spring.learning_spring.models;
+
+public class Employee {
+    private String name, lastname, address, role;
+    private int phone, age;
+
+    public Employee(String name, String lastname, String address, String role, int phone, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.address = address;
+        this.role = role;
+        this.phone = phone;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
