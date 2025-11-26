1cd9a2225f3cf37e74e9608980250eeaf7bc185f
cristians-12 <cristian.sanchez.casd@gmail.com>
2025-11-26 16:28:33 -0500

    feat/ post request for employee

--- src/main/java/com/cristians12/spring/learning_spring/controllers/PathVariableController.java
+++ src/main/java/com/cristians12/spring/learning_spring/controllers/PathVariableController.java
@@ -3,11 +3,14 @@ package com.cristians12.spring.learning_spring.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cristians12.spring.learning_spring.models.Employee;
 import com.cristians12.spring.learning_spring.models.dto.ParamDTO;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/variable")
@@ -17,6 +20,11 @@ public class PathVariableController {
         ParamDTO param1 = new ParamDTO();
         param1.setInformation(param);
         return param1;
+    }
+
+    @PostMapping("/employee")
+    public Employee create(@RequestBody Employee entity) {
+        return entity;
     }
 
 }
